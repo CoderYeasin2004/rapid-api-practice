5d4fb405ed4b2d46f090a55f63b2058f3f2411fc
yeasin <hyeasin59@gmail.com>
2025-12-17 23:02:59 +0600

    added

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-export const baseUrl = "https://realtor-search.p.rapidapi.com"
+export const baseUrl = "https://realtor-search.p.rapidapi.com" // api base url
 
 export const fetchAPI = async (url: string) => {
     const { data } = await axios.get(url, {
         headers: {
-            'X-rapidapi-key': "01b90e76f4msh657ddbc98fe8307p14cb70jsn593cf627ba28",
-            'X-rapidapi-host': "realtor-search.p.rapidapi.com"
+            'X-rapidapi-key': "01b90e76f4msh657ddbc98fe8307p14cb70jsn593cf627ba28", // it's api key
+            'X-rapidapi-host': "realtor-search.p.rapidapi.com" // it's api host
         }
     })
     return data;
